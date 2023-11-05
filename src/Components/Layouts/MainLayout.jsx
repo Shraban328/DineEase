8031e53562965853c62e0a6c8972aac9620c79a2
Shraban Chakma <shrabanchakma328@gmail.com>
2023-11-05 20:03:52 +0600

    added footer

--- src/Components/Layouts/MainLayout.jsx
+++ src/Components/Layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
 
 const MainLayout = () => {
   return (
     <div className="font-montserrat">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
